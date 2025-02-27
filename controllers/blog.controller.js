6cb8c20b76bd00a3bbdc336197a8d7dbc3a73a76
Rupesh Chand <rupeshchandvasantam@gmail.com>
2025-02-27 13:21:28 +0530

    added get blog by id module

--- controllers/blog.controller.js
+++ controllers/blog.controller.js
@@ -1,3 +1,4 @@
+import mongoose from 'mongoose'
 import Blog from '../models/Blog.model.js'
 //for creating blog
 export const createBlog = async (req, res, next) => {
@@ -7,9 +8,9 @@ export const createBlog = async (req, res, next) => {
             title, content, topic, image, ref, location
         })
         await blog.save()
-        res.status(200).json({ succes: true, message: "Blog Saved Successfully" })
+        return res.status(200).json({ succes: true, message: "Blog Saved Successfully" })
     } catch (error) {
-        res.status(500).json({ succes: false, message: "Internal Server Error" })
+        return res.status(500).json({ succes: false, message: "Internal Server Error" })
     }
 }
 //for editing blog
@@ -18,11 +19,30 @@ export const editBlog = async (req, res, next) => {
     try {
         const blog = await Blog.findById(blogId) //for checking id in db
         if (!blog) {
-            res.status(404).json({ success: false, message: "Blog not found" })
+           return res.status(404).json({ success: false, message: "Blog not found" })
         }
         await Blog.findByIdAndUpdate(blogId, { $set: req.body }, { new: true }) //updating data by using findByIdAndUpdate
-        res.status(200).json({ success: true, message: "Blog Updated"}) //"" is not required because at first it obj when it touches express.json() it will be in obje by using json we are telling we need to convert this obj to json format
+        return res.status(200).json({ success: true, message: "Blog Updated" }) //"" is not required because at first it obj when it touches express.json() it will be in obje by using json we are telling we need to convert this obj to json format
     } catch (error) {
+        return res.status(500).json({ success: false, message: "Internal Server Error" })
+    }
+}
+
+//for getting single blog by id
+export const getBlog = async (req, res, next) => {
+    const blogId = req.params.id
+    try {
+        if(!mongoose.Types.ObjectId.isValid(blogId)){
+            return res.status(400).json({success:false,message:"ID not found"})
+        }
+        const blog = await Blog.findById(blogId)
+        if (!blog) {
+            return res.status(404).json({ success: false, message: "Blog not found" })
+        }
+        return res.status(200).json({ success: true, message: "Blog Found" })   
+
+    }
+    catch (error) {
         res.status(500).json({ success: false, message: "Internal Server Error" })
     }
 }
--- routes/blog.route.js
+++ routes/blog.route.js
@@ -1,6 +1,7 @@
 import express from "express"
-import { createBlog, editBlog } from "../controllers/blog.controller.js"
+import { createBlog, editBlog, getBlog } from "../controllers/blog.controller.js"
 const route = express.Router()
 route.post("/createblog", createBlog)
 route.put("/editblog/:id",editBlog)
+route.get("/getblog/:id",getBlog)
 export default route
