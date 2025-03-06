82522985c0046a49dc5085c3b645823d18a66624
Rupesh Chand <rupeshchandvasantam@gmail.com>
2025-03-06 23:29:20 +0530

    added autherization based routing

--- auth/verifyToken.js
+++ auth/verifyToken.js
@@ -1,6 +1,6 @@
 //authenticating the token after user performs login operation
 import jwt from 'jsonwebtoken'
-
+import User from "../models/User.model.js"
 //authenticating the user
 export const authenticate = async (req, res, next) => {
     const authToken = req.headers.authorization;
@@ -13,8 +13,8 @@ export const authenticate = async (req, res, next) => {
         const token = authToken.split(' ')[1]
         //verifying the token if it matches it is going to send user payload
         const validatedToken = jwt.verify(token, process.env.JWT_SECRET_KEY)
-        
-        req.userId = validatedToken.userId
+        console.log("TOken",validatedToken)
+        req.userId = validatedToken.id
         req.role = validatedToken.role
         req.username = validatedToken.username
         next()
@@ -26,4 +26,26 @@ export const authenticate = async (req, res, next) => {
     }
 }
 
-//role based authorization
+//autherizing the user based on roles
+export const restrict = (roles)=> async(req,res,next)=>{
+    try {
+        const userId = req.userId //i am getting it after user loggedin we have this in authenticate
+        console.log(userId)
+        const user = await User.findById(userId) //that id i'm checking in db whether it is there or not
+        //here no need of checking the error again bcse this id we got after authenticating \
+        const userRole = user.role
+        //for user here i am checking two conditions if user role is user and roles which we are receiving from calling the function contains user or not  
+        if(userRole === "user" && roles.includes("user")){
+            next()
+        }
+        //for admin
+        else if(userRole === "admin" && roles.includes("admin")){
+            next()
+        }
+        else{
+            return res.status(401).json({success:false,message: `${userRole} is not authorized to access this data`})
+        }
+    } catch (error) {
+        return res.status(500).json({success:false,message:"Internal Server Error"})        
+    }
+}
--- routes/blog.route.js
+++ routes/blog.route.js
@@ -1,13 +1,13 @@
 import express from "express"
 import { createBlog, deleteBlog, editBlog, getBlog, getBlogByQueryTopic, getBlogByTopic, getBlogs } from "../controllers/blog.controller.js"
-import { authenticate } from "../auth/verifyToken.js"
+import { authenticate, restrict } from "../auth/verifyToken.js"
 const route = express.Router()
 route.post("/createblog", authenticate, createBlog)
 route.put("/editblog/:id", authenticate, editBlog)
 route.get("/getsingleblog/:id", getBlog)
 route.get("/getblogs", getBlogs)
 route.get("/getblogbytopic/:topic", getBlogByTopic)
-route.delete("/deleteblog/:id", deleteBlog)
+route.delete("/deleteblog/:id", authenticate, restrict(["admin"]), deleteBlog)
 
 //http://localhost:8080/api/v1/blog/blogbyquery/?topic=js
 route.get("/blogbyquery", getBlogByQueryTopic)
