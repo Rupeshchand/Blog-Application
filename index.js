cb54253bdd3afd01892bf028e1cf5eaf4ca0f3f5
Rupesh Chand <rupeshchandvasantam@gmail.com>
2025-03-14 00:23:51 +0530

    added default cors method in index file

--- index.js
+++ index.js
@@ -31,13 +31,28 @@ const port = process.env.PORT || 8080
 //     origin: true, // it will take origin automatically and no need of mentioning methods
 //     credentials: true
 // }))
-const corsOptions = {
-    origin: "*",
-    credentials: true,
-    optionSuccessStatus: 200,
-  };
+// const corsOptions = {
+//     origin: ["*","http://localhost:5173"],
+//     credentials: true,
+//     optionSuccessStatus: 200,
+//   };
   
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+
+const allowedLinks = ["http://localhost:5173"]
+app.use(cors({
+    origin: function(origin,callback){
+        if(!origin || allowedLinks.includes(origin)){
+            callback(null,true)
+        }
+        else{
+            callback( new Error("Not allowed by CORS"))
+        }
+    },
+    credentials: true,
+    methods:["GET","POST","PUT","DELETE"],
+    allowedHeaders: ["Content-Type","Autherization"]
+}))
 app.use(cookieParser()) //before server is starting cookies will be parsed
 app.use(express.json()) //to parse json data
 app.use(express.static("public"))
